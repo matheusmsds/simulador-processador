0b5704fbddb76344727f0baaa79ad3bad5ac4aea
CaioTanaka <20251PVAI10030030@estudantes.ifpr.edu.br>
2026-08-04 20:51:30 -0300

    Starting with the CPU operations and refining the decode

---
+++ arq/cpuMono.h
@@ -0,0 +1,22 @@
+#ifndef CPU_FUNCOES_H
+#define CPU_FUNCOES_H
+
+#include <stdint.h>
+
+typedef struct
+{
+    uint16_t tipoInstrucao;          // Tipo da instrução
+    uint16_t opcode;                 // Opcode
+
+    // Para instruções do tipo R
+    uint16_t operando1;              // Operador 1 - "x"
+    uint16_t operando2;              // Operador 2 - "y"
+    uint16_t regDestino;     // Registrador que será salvo
+
+    // Para instruções do tipo I
+    uint16_t imediato;               // Registrador imediato das instruções do tipo I
+    uint16_t regSalto;       // Caso jump ignora, se jump_cond será executado ou não
+
+} InstrucaoDecodificada;
+
+#endif
